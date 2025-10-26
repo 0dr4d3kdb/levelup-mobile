9b74be6b58874d3c7eff7012a5a1748532efd10f
0dr4d3kdb <danielbeta97@gmail.com>
2025-10-25 22:16:05 -0300

    Creación inicial pantalla HomeScreen

--- app/src/main/java/com/example/level_upmovil/MainActivity.kt
+++ app/src/main/java/com/example/level_upmovil/MainActivity.kt
@@ -6,31 +6,34 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.level_upmovil.ui.screens.HomeScreen
 import com.example.level_upmovil.ui.theme.LevelupMovilTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContent {
             LevelupMovilTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    color = MaterialTheme.colorScheme.surface
+                ) {
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
+
+/*@Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
@@ -44,4 +47,4 @@ fun GreetingPreview() {
     LevelupMovilTheme {
         Greeting("Android")
     }
-}
+}*/
---
+++ app/src/main/java/com/example/level_upmovil/ui/screens/HomeScreen.kt
@@ -0,0 +1,58 @@
+package com.example.level_upmovil.ui.screens
+
+import com.example.level_upmovil.R
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(){
+    Scaffold (
+        topBar = {
+            CenterAlignedTopAppBar(title = {Text("Level-up gamer")})
+        }
+    ){ innerPadding ->
+        Column (
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(20.dp)
+        ){
+            Text(text = "¡Bienvenido!")
+            Button(onClick = {}) {
+                Text("Presióname")
+            }
+            Image(
+                painter = painterResource(R.drawable.level),
+                contentDescription = "Logo app",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(150.dp),
+                contentScale = ContentScale.Fit
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
